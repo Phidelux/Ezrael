1418c4a7153c725de9dcedbb98a71bdacbed247b
Avedo <info@avedo.net>
2015-05-16 22:54:07 +0200

    Fixed bug in rejoin plugin.

--- plugins/rejoin.py
+++ plugins/rejoin.py
@@ -4,7 +4,7 @@ import time
 
 class Rejoin(Plugin):
     def onKick(self, irc, channel, nick, msg):
-        print ("NOTICE: I was kicked by {}0 from {1}\r\n".format(nick, channel))
+        print ("NOTICE: I was kicked by {0} from {1}\r\n".format(nick, channel))
         irc.joinChannel(channel)
         print ("NOTICE: Rejoining {0} ...".format(channel))
         irc.sendPlain("MODE {0} -o {1} \r\n".format(channel, nick).encode())
