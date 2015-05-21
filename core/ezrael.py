edc886b48454a02442b95a4b5d62e2405126da44
frissdiegurke <frissdiegurke@hotmail.com>
2015-05-21 18:13:53 +0200

    #3 fixed.

--- core/ezrael.py
+++ core/ezrael.py
@@ -160,7 +160,7 @@ class Ezrael(object):
                     continue
                 # ... generate Message-object, ...
                 message = Message(self, m)
-                print("Received %s" % message)
+                print("Received %s" % str(message).encode('latin-1'))
                 # ... run built-in commands and ...
                 if len(message.cmd):
                     self.check_commands(message)
