c4d0ca48f58d06d0b2c4ac553fc4d292cab01e5f
Andreas Wilhelm <awilhelm@uni-math.gwdg.de>
2015-06-23 20:35:06 +0200

    Fixed Fortune Plugin issue #4.

--- plugins/fortune.py
+++ plugins/fortune.py
@@ -7,29 +7,34 @@ class Fortune(Plugin):
         if message.cmd[0] != 'fortune':
             return
 
-        # Setup the request url, ...
-        url = 'http://www.iheartquotes.com/api/v1/random' \
-              + '?show_permalink=false' \
-              + '&max_characters=200' \
-              + '&format=text'
+        try:
+            # Setup the request url, ...
+            url = 'http://www.iheartquotes.com/api/v1/random' \
+                  + '?show_permalink=false' \
+                  + '&max_characters=200' \
+                  + '&format=text'
 
-        # ... fetch a fortune from http://iheartquotes.com, ...
-        fortune = urllib.request.urlopen(url).read()
+            # ... fetch a fortune from http://iheartquotes.com, ...
+            fortune = urllib.request.urlopen(url).read()
 
-        # ... remove linebreaks and tabs, ...
-        fortune = str(fortune, 'utf-8') \
-            .replace('\r', '') \
-            .replace('\n', ' ') \
-            .replace('\t', ' ')
+            # ... remove linebreaks and tabs, ...
+            fortune = str(fortune, 'utf-8') \
+                .replace('\r', '') \
+                .replace('\n', ' ') \
+                .replace('\t', ' ')
 
-        # ... replace special characters ...
-        special = {
-            '&nbsp;': ' ', '&amp;': '&', '&quot;': '"',
-            '&lt;': '<', '&gt;': '>'
-        }
+            # ... replace special characters ...
+            special = {
+                '&nbsp;': ' ', '&amp;': '&', '&quot;': '"',
+                '&lt;': '<', '&gt;': '>'
+            }
 
-        for (k, v) in special.items():
-            fortune = fortune.replace(k, v)
+            for (k, v) in special.items():
+                fortune = fortune.replace(k, v)
 
-        # ... and send it as message to the irc channel.
-        irc.send_message('[Fortune] ' + fortune, message.channel)
+            # ... and send it as message to the irc channel.
+            irc.send_message('[Fortune] ' + fortune, message.channel)
+        except urllib.error.HTTPError, e:
+            irc.send_message('[Fortune] Service currently unavailable', message.channel)
+        except Exception:
+            pass
