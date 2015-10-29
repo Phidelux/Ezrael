ac88d280659444ab71939ab52fac12b5c0f45491
Bornageek <info@bornageek.com>
2015-10-29 16:00:45 +0100

    Extended HereAmI plugin.

--- plugins/hereami.py
+++ plugins/hereami.py
@@ -1,10 +1,16 @@
 from core.plugin import Plugin
-
+import random
 
 class HereAmI(Plugin):
+    hereami = [
+      "Are you talking to me?", "Talk to the hand!", "Here am I!", 
+      "You wanna hug?", "Hello there!"
+    ]
+
     def on_message(self, irc, message):
         if len(message.cmd) == 0 and 'ezrael' in message.content.lower():
-            irc.send_message('Here am I!', message.channel)
+            hereMsg = HereAmI.hereami[random.randint(0, len(HereAmI.hereami) - 1)]
+            irc.send_message(hereMsg, message.channel)
 
     def on_join(self, irc, message):
         if message.nick == irc.ircNick:
