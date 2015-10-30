decf6f51e016ba95984fc3d76c22de37cdc3adab
Bornageek <info@bornageek.com>
2015-10-30 14:46:10 +0100

    Updated rejoin plugin to use new messaging syntax.

--- plugins/rejoin.py
+++ plugins/rejoin.py
@@ -2,8 +2,8 @@ from core.plugin import Plugin
 
 
 class Rejoin(Plugin):
-    def on_kick(self, irc, message):
+    def on_kick(self, message):
         print("NOTICE: I was kicked by {0} from {1}\r\n".format(message.nick, message.channel))
-        irc.join_channel(message.channel)
+        self.join_channel(message.channel)
         print("NOTICE: Rejoining {0} ...".format(message.channel))
-        irc.send("MODE {0} -o {1} \r\n".format(message.channel, message.nick).encode())
+        self.send("MODE {0} -o {1} \r\n".format(message.channel, message.nick).encode())
