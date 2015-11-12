dfdae0839ed8c8c04b1221206e479a232560b7e8
Bornageek <info@bornageek.com>
2015-11-12 16:32:48 +0100

    Basic version of notes plugin!

--- plugins/notes.py
+++ plugins/notes.py
@@ -3,14 +3,14 @@ from collections import namedtuple
 import time
 
 # Define a Note struct.
-Note = namedtuple("Note", "time sender message")
+Note = namedtuple("Note", "timestamp sender message")
 
 class Notes(Plugin):
     def init(self):
         # Initialize the message stack.
         self.notes = {}
 
-    def save(self, note):
+    def save(self, nick, note):
         if nick not in self.notes.keys():
             self.notes[nick] = []
 
@@ -18,17 +18,39 @@ class Notes(Plugin):
 
     def clear(self, nick):
         if nick in self.notes.keys():
-            self.notes[nick] = []
+            del self.notes[nick]
 
     def on_command(self, message):
         if message.cmd[0] not in ['note', 'mynotes']:
             return
 
-        if message.cmd[0] == 'mynotes' and message.nick in self.notes.keys():
-            for note in self.notes[message.nick]:
-                self.send_message("{0}: {1} | {2}".format(
-                        note.time, note.sender, note.message))
+        if message.cmd[0] == 'mynotes':
+            if message.nick in self.notes.keys():
+                for note in self.notes[message.nick]:
+                    self.send_message("{0}: {1} | {2}".format(
+                          note.timestamp, note.sender, note.message), message.channel)
 
-            self.clear(message.nick)
+                self.clear(message.nick)
+            else:
+                self.send_message('No notes for you!', message.channel)
         elif message.cmd[0] == 'note':
-            pass
+            # Split the message in command, username and message ...
+            words = message.content.split()
+
+            # ... and check if a nickname and message were given.
+            if len(words) < 3:
+                return
+
+            # Fetch the given nickname, ...
+            nickname = words[1].strip()
+
+            # ... extract the message, ...
+            msgText = ' '.join(words[2:])
+
+            # ... fetch the current timestamp ...
+            timestamp = time.strftime("%d. %b %Y, %H:%M")
+
+            print("{0}: {1} -> {2} | {3}".format(timestamp, message.nick, nickname, msgText))
+
+            # ... and save the new note.
+            self.save(nickname, Note(timestamp, message.nick, msgText))
